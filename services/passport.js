17a7357db3796f13edfccf6394a33c3d7e00b932
Therk <therk@me.com>
2018-03-06 21:07:54 -0500

    refactored futures to async await syntax

--- services/passport.js
+++ services/passport.js
@@ -21,18 +21,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          console.info('User already exist ' + profile.id);
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-          console.info('User created with ' + profile.id);
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        console.info('User already exist ' + profile.id);
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+      console.info('User created with ' + profile.id);
     }
   )
 );
